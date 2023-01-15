111a05dc38bc715ea8f55083a67daf0742e0d2a9
gordon-hj <gordon_hj@naver.com>
2023-01-15 18:10:29 +0900

    clear git cache

--- ch2_smallest_client.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-const url = 'http://localhost:80/api/get';
-const res = fetch(url).then(res => {
-    // console.log(res);
-    res.text().then(txt => {
-        console.log(txt);
-    })
-})
